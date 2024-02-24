dca7941002d2cef58ed2fdaa62500f64dc3a2e43
Natia Kevlishvili <161031948+Nat-kevlika@users.noreply.github.com>
2024-02-24 15:24:33 +0400

    Delete Homework_1 directory

--- Homework_1/task_1.py
+++
@@ -1,4 +0,0 @@
-Name = input("Your name? ")
-Lname = input("Your last name? ")
-Age = int(input("Your age? "))
-print( "Hello", Name, Lname, "your age is", Age )
--- Homework_1/task_2.py
+++
@@ -1,6 +0,0 @@
-Name = input(" Your name? ")
-Byear = int(input(" Your year of birth? "))
-Year = 2024
-Age = Year - Byear
-
-print("Hello", Name, "your age is", Age)
