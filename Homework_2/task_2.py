1783fd82e23f2a156263a8c8fd6e2d67f2f9b4df
Nat-kevlika <kevlishvili.natia@tbcacademy.edu.ge>
2024-03-05 19:08:29 +0400

    the last version

--- Homework_2/task_2.py
+++ Homework_2/task_2.py
@@ -1,23 +1,27 @@
 ####### version 1 ###############
-Numb_1 = int(input("Enter the first number: "))
-Numb_2 = int(input("Enter the second number: "))
+numb_1 = int(input("Enter the first number: "))
+numb_2 = int(input("Enter the second number: "))
 
-if Numb_1 % Numb_2 == 0:
-    print(Numb_1, " is a multiple of ", Numb_2 )
+if numb_1 % numb_2 == 0:
+    print(numb_1, " is a multiple of ", numb_2)
 else:
-    print(Numb_1, " is not a multiple of ", Numb_2)
+    print(numb_1, " is not a multiple of ", numb_2)
 
 
 ########## version 2 ###################
-Numb_1 = int(input("Enter the first number: "))
-Numb_2 = int(input("Enter the second number: "))
-nat_numb = [n for n in range(1, Numb_1+1)]
+numb_1 = int(input("Enter the first number: "))
+numb_2 = int(input("Enter the second number: "))
+nat_numb = [n for n in range(1, numb_1+1)]
 print(nat_numb)
 
-if Numb_1 in [Numb_2 * n for n in nat_numb]:
-    print(Numb_1, "is a multiple of ", Numb_2)
+if numb_1 in [numb_2 * n for n in nat_numb]:
+    print(numb_1, "is a multiple of ", numb_2)
 else:
-    print(Numb_1, "is not a multiple of ", Numb_2)
+    print(numb_1, "is not a multiple of ", numb_2)
+
+
+
+
 
 
 
