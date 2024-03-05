56f99651a1894cf7861d3262706df0206aa6a1f8
Nat-kevlika <kevlishvili.natia@tbcacademy.edu.ge>
2024-03-05 18:40:57 +0400

    the last version

--- Homework_1/task_2.py
+++ Homework_1/task_2.py
@@ -1,6 +1,6 @@
-Name = input(" Your name? ")
-Byear = int(input(" Your year of birth? "))
-Year = 2024
-Age = Year - Byear
+name = input(" Your name? ")
+year_of_birth = int(input(" Your year of birth? "))
+year_now = 2024
+age = year_now - year_of_birth
 
-print("Hello", Name, "your age is", Age)
+print("Hello", name, "your age is", age)
